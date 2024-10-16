2025fc9fc1ee5cbac9537dd4c9f921e0d8540a52
YGueguen16u <yann.gueguen.company@gmail.com>
2024-10-16 12:47:53 +0200

    modify proba of high quantity

--- fake_data/sensor.py
+++ fake_data/sensor.py
@@ -107,7 +107,7 @@ class User:
         # 5% de chances que la quantité soit nulle
         if random.random() < 0.04:
             return 0
-        elif random.random() > 0.99:
+        elif random.random() > 0.995:
             return random.randint(100, 100000)
 
         quantite = 1  # Quantité par défaut
@@ -151,7 +151,7 @@ class User:
         total_calories = 0
         aliments_selectionnes = []
 
-        exceed_max_calories = random.random() < 0.3  # 30% de chances de pouvoir dépasser max_calories
+        exceed_max_calories = random.random() < 0.2  # 30% de chances de pouvoir dépasser max_calories
 
         for type_aliment in types_choisis:
             aliments_du_type = aliments_df[aliments_df['Type'] == type_aliment]
